bcaae6d7e0c345dabfef178c1cc17abfcbaf69a6
zxk901021 <447360581@qq.com>
2016-06-12 18:07:24 +0800

    ui detail 20160608

--- app/src/main/java/com/zhy_9/edusoho/com/zhy_9/edusoho/activity/MainActivity.java
+++ app/src/main/java/com/zhy_9/edusoho/com/zhy_9/edusoho/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.zhy_9.edusoho.R;
 import com.zhy_9.edusoho.adapter.HomePagerAdapter;
@@ -19,6 +21,8 @@ public class MainActivity extends FragmentActivity {
     private ViewPager homePager;
     private HomePagerAdapter adapter;
     private ArrayList<Fragment> fragments;
+    private ImageView homePageImg, courseImg, evaluationImg, personImg;
+    private TextView homePageTv, courseTv, evaluationTv, personTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,76 @@ public class MainActivity extends FragmentActivity {
 
     public void initView(){
         homePager = (ViewPager) findViewById(R.id.homepage_pager);
+        homePageImg = (ImageView) findViewById(R.id.home_img);
+        homePageTv = (TextView) findViewById(R.id.home_tv);
+        courseImg = (ImageView) findViewById(R.id.course_img);
+        courseTv = (TextView) findViewById(R.id.course_tv);
+        evaluationImg = (ImageView) findViewById(R.id.evaluation_img);
+        evaluationTv = (TextView) findViewById(R.id.evaluation_tv);
+        personImg = (ImageView) findViewById(R.id.person_img);
+        personTv = (TextView) findViewById(R.id.person_tv);
+        homePager.setCurrentItem(0);
+        homePager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                setHomeBottomMenuSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+    public void setHomeBottomMenuSelected(int position){
+        switch (position) {
+            case 0:
+                homePageImg.setImageResource(R.drawable.home_icon);
+                homePageTv.setTextColor(getResources().getColor(R.color.home_munu_text_selected));
+                courseImg.setImageResource(R.drawable.course_icon);
+                evaluationImg.setImageResource(R.drawable.evaluate_icon);
+                personImg.setImageResource(R.drawable.member_center_icon);
+                courseTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                evaluationTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                personTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                break;
+            case 1:
+                homePageImg.setImageResource(R.drawable.home_icon_unselected);
+                homePageTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                courseImg.setImageResource(R.drawable.course_icon_selected);
+                evaluationImg.setImageResource(R.drawable.evaluate_icon);
+                personImg.setImageResource(R.drawable.member_center_icon);
+                courseTv.setTextColor(getResources().getColor(R.color.home_munu_text_selected));
+                evaluationTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                personTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                break;
+            case 2:
+                homePageImg.setImageResource(R.drawable.home_icon_unselected);
+                homePageTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                courseImg.setImageResource(R.drawable.course_icon);
+                evaluationImg.setImageResource(R.drawable.evaluate_icon_selected);
+                personImg.setImageResource(R.drawable.member_center_icon);
+                courseTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                evaluationTv.setTextColor(getResources().getColor(R.color.home_munu_text_selected));
+                personTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                break;
+            case 3:
+                homePageImg.setImageResource(R.drawable.home_icon_unselected);
+                homePageTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                courseImg.setImageResource(R.drawable.course_icon);
+                evaluationImg.setImageResource(R.drawable.evaluate_icon);
+                personImg.setImageResource(R.drawable.member_center_icon_selected);
+                courseTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                evaluationTv.setTextColor(getResources().getColor(R.color.home_menu_text));
+                personTv.setTextColor(getResources().getColor(R.color.home_munu_text_selected));
+                break;
+        }
+
     }
 
     public void initAdapter(){
